9594338f52e76d829ab7c0f331636403eae44fee
anlikel <antongubenko2@gmail.com>
2025-09-11 17:04:15 +0300

    добавил в 2 сделанных класса интерфейс comparable для естественной сортировки по умолчанию(нулевой клмпаратор)

--- Komanda4/src/utils/TestCollections.java
+++ Komanda4/src/utils/TestCollections.java
@@ -1,13 +1,32 @@
 package utils;
 
+import classes.Product;
+import classes.User;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestCollections {
     public static void main(String[] args) {
-        List<Integer> list=new MyArrayListImpl<>();
-        list.addAll(List.of(1,5,7,3,65,1,99));
-        list.sort(null);
-        list.forEach(System.out::println);
+        List<Product> products=new MyArrayListImpl<>();
+        products.add(new Product.ProductBuilder()
+                .setTitle("Laptop")
+                .setPrice(999.99)
+                .setQuantity(5)
+                .build());
+
+        products.add(new Product.ProductBuilder()
+                .setTitle("Mouse")
+                .setPrice(25.50)
+                .setQuantity(20)
+                .build());
+
+        products.add(new Product.ProductBuilder()
+                .setTitle("Laptop") // Такое же название как у первого
+                .setPrice(899.99)   // Но другая цена
+                .setQuantity(3)
+                .build());
+        products.sort(null);
+        products.forEach(System.out::println);
     }
 }
