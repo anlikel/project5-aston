466a062c4a6c2d99ffc1583c79891ecc8b67beae
anlikel <antongubenko2@gmail.com>
2025-09-12 10:45:52 +0300

    add sort with comapare and comparator

--- Komanda4/src/menu/SortMenuHandler.java
+++ Komanda4/src/menu/SortMenuHandler.java
@@ -1,12 +1,16 @@
 package menu;
 
 import commands.CommandFactory;
+import comparators.ComparatorFabric;
+import comparators.Filter;
 import controller.Controller;
 import enums.Action;
+import enums.ClassTags;
 import exceptions.ReadWriteException;
 import model.Model;
 import utils.Holder;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static utils.Util.writeMessage;
@@ -15,15 +19,21 @@ public class SortMenuHandler {
     public static void mainSortMenuHandler(String choose) throws ReadWriteException {
         Controller controller= Holder.getController();
         Model model= controller.getModel();
+        List list=model.getList();
         switch(choose){
             case "1":
-                List list=model.getList();
                 list.sort(null);
                 model.setList(list);
                 controller.setModel(model);
                 break;
             case "2":
-                writeMessage("2-Сортировка по выбору (Comparator)");
+                ClassTags tag=model.getTag();
+                Comparator comp=ComparatorFabric.getComparator(tag);
+                Filter f=(Filter)comp;
+                f.setFilter();
+                list.sort((Comparator)f);
+                model.setList(list);
+                controller.setModel(model);
                 break;
             case "3":
                 CommandFactory.getCommand(Action.EXIT).execute();
