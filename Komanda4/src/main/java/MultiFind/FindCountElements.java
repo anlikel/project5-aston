2e26cd30f9f80177f8dab5ccafced31d89bcac11
AZ1961T <andrei.302@yandex.ru>
2025-09-14 00:45:35 +0500

    add class FindCountElements 14/09/25 AZ1961T

---
+++ Komanda4/src/main/java/MultiFind/FindCountElements.java
@@ -0,0 +1,74 @@
+package MultiFind;
+import utils.Util;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class FindCountElements<T> {
+    private int countThread;
+    private AtomicInteger countElement;
+
+    private List<T> listElements;
+
+    private T element;
+
+    public FindCountElements(List<T> list, T element){
+        this.listElements = list;
+        this.countThread = Runtime.getRuntime().availableProcessors();
+        this.element = element;
+        this.countElement = new AtomicInteger(0);
+    }
+
+    public  void find() throws InterruptedException {
+        int start = 0;
+        int size = 0;
+        if(listElements != null) {
+             size = listElements.size();
+        }
+
+        if(size == 0){
+            Util.writeMessage("Коллекция пуста");
+            return;
+        }
+
+        int fin = size / countThread;
+        if(size <= countThread) {
+            fin = size;
+            countThread = 1;
+        }
+
+        CountDownLatch latch = new CountDownLatch(countThread);
+        for (int i = 0; i < countThread; i++) {
+            int st = start;
+            int fn ;
+            if (countThread - 1 == i) {
+                fn = size ;
+            }
+            else {
+                fn = start + fin;
+            }
+
+            Thread thread = new Thread(()->{
+                try {
+                    forCaunt(st, fn);
+                }finally {
+                    latch.countDown();
+                }
+            });
+            thread.start();
+
+            start = fn;
+
+        }
+        latch.await();
+        Util.writeMessage("Количество вхождения элемента в коллекцию - " + countElement);
+    }
+
+    private void forCaunt(int start, int fin){
+        for(int i = start; i < fin; i++){
+            if(listElements.get(i).equals(element)){
+                    countElement.incrementAndGet();
+            }
+        }
+    }
+}
