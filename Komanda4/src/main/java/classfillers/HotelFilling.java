8a162863bfcaa06598cc1f210dcdb431b621ad19
ArtN0V <artemnovikov02@yandex.ru>
2025-09-14 21:55:52 +0300

    add HotelFilling without autoFill

---
+++ Komanda4/src/main/java/classfillers/HotelFilling.java
@@ -0,0 +1,60 @@
+package classfillers;
+
+import classes.Hotel;
+import classes.User;
+import exceptions.ReadWriteException;
+import utils.MyArrayListImpl;
+import utils.Util;
+
+import java.util.List;
+import java.util.Random;
+
+public class HotelFilling implements Filling<Hotel> {
+    private String name;
+    private String city;
+    private double rating;
+    private int fieldCount = 3;
+
+    @Override
+    public List<Hotel> manualFill(int size) throws ReadWriteException {
+        List<Hotel> hotels = new MyArrayListImpl<>();
+        for (int i = 0; i < size; i++) {
+            Util.writeMessage("input hotel name");
+            name = Util.readMessage();
+
+            Util.writeMessage("input hotel city");
+            city = Util.readMessage();
+
+            Util.writeMessage("input hotel rating");
+            rating = Double.parseDouble(Util.readMessage());
+
+            Hotel hotel = new Hotel.HotelBuilder()
+                    .setName(name)
+                    .setCity(city)
+                    .setRaiting(rating)
+                    .build();
+            hotels.add(hotel);
+        }
+        return hotels;
+    }
+
+    @Override
+    public List<Hotel> randomFill(int size) {
+        List<Hotel> hotels = new MyArrayListImpl<>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            Hotel hotel = new Hotel.HotelBuilder()
+                    .setName("Hotel" + random.nextInt(1000))
+                    .setCity("Hotel" + random.nextInt(1000))
+                    .setRaiting(random.nextDouble(5))
+                    .build();
+            hotels.add(hotel);
+        }
+        return hotels;
+    }
+
+    @Override
+    public List<Hotel> autoFill() throws ReadWriteException {
+        return List.of();
+    }
+}
