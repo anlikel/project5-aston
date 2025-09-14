1720b8b9734e2e6d3e7bcec0d7903156efb76fb7
anlikel <antongubenko2@gmail.com>
2025-09-14 13:33:48 +0300

    add write command with normal and append mode

--- Komanda4/src/main/java/menu/ReadWriteMenuPrinter.java
+++ Komanda4/src/main/java/menu/ReadWriteMenuPrinter.java
@@ -3,7 +3,6 @@ package menu;
 import exceptions.ReadWriteException;
 import utils.Util;
 
-import java.util.List;
 
 public class ReadWriteMenuPrinter {
     public static void mainReadMenu() throws ReadWriteException {
@@ -11,7 +10,14 @@ public class ReadWriteMenuPrinter {
         Util.writeMessage("1-Чтение из файла по умолчанию");
         Util.writeMessage("2-Чтение из определенного файла");
     }
+
     public static void mainWriteMenu() throws ReadWriteException {
+        Util.writeMessage("0-Выход");
+        Util.writeMessage("1-Перезапись файла");
+        Util.writeMessage("2-Дозапись в конец файла");
+    }
+
+    public static void mainWriteTargetMenu() throws ReadWriteException {
         Util.writeMessage("0-Выход");
         Util.writeMessage("1-Запись в файл по умолчанию");
         Util.writeMessage("2-Запись в определенный файл");
