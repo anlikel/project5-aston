732f4256d1af174fd1c40a79f1dfb2c64995879e
anlikel <antongubenko2@gmail.com>
2025-09-15 11:17:06 +0300

    встроены методыы поиск количества элементов и создание элемента

--- Komanda4/src/main/java/commands/FindComand.java
+++ Komanda4/src/main/java/commands/FindComand.java
@@ -2,8 +2,10 @@ package commands;
 
 import classes.Product;
 import classes.User;
+import controller.Controller;
 import enums.ClassTags;
 import exceptions.ReadWriteException;
+import menu.FindMenuPrinter;
 import model.Model;
 import multiFind.FindCountElements;
 import utils.Holder;
@@ -13,15 +15,11 @@ import java.util.List;
 public class FindComand  implements Command{
     @Override
     public void execute() throws ReadWriteException {
-        //Логика для получения элемента
-
-        // var el = Holder.getController().getModel().getTag();
-        User element= null;//заглушка
-        List<User> userList = null;
-        try {
-            FindCountElements.find(userList, element);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        Controller controller= Holder.getController();
+        Model model=controller.getModel();
+        if(model==null){
+            throw new ReadWriteException("Исключение: Сначала необходимо создать коллекцию для поиска");
         }
+        FindMenuPrinter.mainFindMenu();
     }
 }
