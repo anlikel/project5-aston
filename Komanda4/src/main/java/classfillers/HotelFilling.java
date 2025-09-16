0684b335982d96081dd0488f75e1aa5122b9f399
anlikel <antongubenko2@gmail.com>
2025-09-16 13:37:16 +0300

    fix sort commands

--- Komanda4/src/main/java/classfillers/HotelFilling.java
+++ Komanda4/src/main/java/classfillers/HotelFilling.java
@@ -12,6 +12,8 @@ import utils.Util;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class HotelFilling implements Filling<Hotel> {
     private String name;
@@ -20,41 +22,85 @@ public class HotelFilling implements Filling<Hotel> {
 
     @Override
     public List<Hotel> manualFill(int size) throws ReadWriteException {
-        List<Hotel> hotels = new MyArrayListImpl<>();
+//        List<Hotel> hotels = new MyArrayListImpl<>();
+//        for (int i = 0; i < size; i++) {
+//            Util.writeMessage("input hotel name or exit");
+//            name = Util.readName();
+//
+//            Util.writeMessage("input hotel city or exit");
+//            city = Util.readCity();
+//
+//            Util.writeMessage("input hotel rating or exit");
+//            String rating1=Util.readRating();
+//            rating = Double.parseDouble(rating1);
+//
+//            Hotel hotel = new Hotel.HotelBuilder()
+//                    .setName(name)
+//                    .setCity(city)
+//                    .setRaiting(rating)
+//                    .build();
+//            hotels.add(hotel);
+//        }
+//        return hotels;
+//        ********************************************************************************
+        String[] names = new String[size];
+        String[] cities = new String[size];
+        double[] ratings = new double[size];
+
         for (int i = 0; i < size; i++) {
             Util.writeMessage("input hotel name or exit");
-            name = Util.readName();
+            names[i] = Util.readName();
 
             Util.writeMessage("input hotel city or exit");
-            city = Util.readCity();
+            cities[i] = Util.readCity();
 
             Util.writeMessage("input hotel rating or exit");
-            String rating1=Util.readRating();
-            rating = Double.parseDouble(rating1);
-
-            Hotel hotel = new Hotel.HotelBuilder()
-                    .setName(name)
-                    .setCity(city)
-                    .setRaiting(rating)
-                    .build();
-            hotels.add(hotel);
+            String ratingStr = Util.readRating();
+            ratings[i] = Double.parseDouble(ratingStr);
         }
-        return hotels;
+
+        return IntStream.range(0, size)
+                .mapToObj(i -> new Hotel.HotelBuilder()
+                        .setName(names[i])
+                        .setCity(cities[i])
+                        .setRaiting(ratings[i])
+                        .build())
+                .collect(Collectors.toCollection(MyArrayListImpl::new));
     }
 
     @Override
     public List<Hotel> randomFill(int size) {
-        List<Hotel> hotels = new MyArrayListImpl<>();
+//        List<Hotel> hotels = new MyArrayListImpl<>();
+//        Random random = new Random();
+//        for (int i = 0; i < size; i++) {
+//            Hotel hotel = new Hotel.HotelBuilder()
+//                    .setName("Hotel" + random.nextInt(1000))
+//                    .setCity("City" + random.nextInt(1000))
+//                    .setRaiting(random.nextDouble(5))
+//                    .build();
+//            hotels.add(hotel);
+//        }
+//        return hotels;
+//        ************************************************************
+        String[] names = new String[size];
+        String[] cities = new String[size];
+        double[] ratings = new double[size];
         Random random = new Random();
+
         for (int i = 0; i < size; i++) {
-            Hotel hotel = new Hotel.HotelBuilder()
-                    .setName("Hotel" + random.nextInt(1000))
-                    .setCity("City" + random.nextInt(1000))
-                    .setRaiting(random.nextDouble(5))
-                    .build();
-            hotels.add(hotel);
+            names[i] = "Hotel" + random.nextInt(1000);
+            cities[i] = "City" + random.nextInt(1000);
+            ratings[i] = random.nextDouble() * 5; // Рейтинг от 0.0 до 5.0
         }
-        return hotels;
+
+        return IntStream.range(0, size)
+                .mapToObj(i -> new Hotel.HotelBuilder()
+                        .setName(names[i])
+                        .setCity(cities[i])
+                        .setRaiting(ratings[i])
+                        .build())
+                .collect(Collectors.toCollection(MyArrayListImpl::new));
+
     }
 
     @Override
--- Komanda4/src/main/java/commands/SortCommand.java
+++ Komanda4/src/main/java/commands/SortCommand.java
@@ -1,10 +1,29 @@
 package commands;
 
+import exceptions.ReadWriteException;
+import menu.MenuPrinter;
 import menu.SortMenuPrinter;
+import utils.Holder;
+import utils.Util;
+
+import java.util.List;
 
 public class SortCommand implements Command{
     @Override
     public void execute() {
+        try {
+            if (Holder.getController().getModel() == null) {
+                throw new ReadWriteException("исключение: модель не инициализирована");
+            }
+            List list = Holder.getController().getList();
+            if (list == null || list.isEmpty()) {
+                throw new ReadWriteException("исключение: лист пустой нечего сортировать");
+            }
+        }
+        catch(ReadWriteException e){
+            Util.writeMessage(e.getMessage().toString());
+            MenuPrinter.mainMenu();
+        }
         SortMenuPrinter.mainSortMenu();
     }
 }
