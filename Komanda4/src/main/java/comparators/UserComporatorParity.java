d9221ee0ed1c121cee309073e8733192290d4268
AZ1961T <andrei.302@yandex.ru>
2025-09-17 17:22:30 +0500

    add ComparatorFabricParity 17/09/25

---
+++ Komanda4/src/main/java/comparators/UserComporatorParity.java
@@ -0,0 +1,33 @@
+package comparators;
+
+import classes.User;
+
+import java.util.Comparator;
+
+public class UserComporatorParity implements Comparator<User> {
+    @Override
+    public int compare(User o1, User o2) {
+        if (o1.getAge() % 2 == 0 && o2.getAge() % 2 == 0) {
+
+                if ( o1.getAge() > o2.getAge()) {
+                    return 1;
+                } else if ( o1.getAge() < o2.getAge()) {
+                    return -1;
+                }
+                return 0;
+        }
+
+        if (o1.getAge() % 2 != 0 && o2.getAge() % 2 != 0) {
+            return 0;
+        }
+
+        if (o1.getAge() % 2 == 0 && o2.getAge() % 2 != 0) {
+            return -1;
+        }
+
+        return 1;
+    }
+
+
+
+}
