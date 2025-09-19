b45b812c63c4798bc55bea8975183bdbe9279c30
AZ1961T <andrei.302@yandex.ru>
2025-09-19 16:40:16 +0500

    add tests ComporatorParity 19/09/25

---
+++ Komanda4/src/test/java/Comporators/HotelComporatorParityTest.java
@@ -0,0 +1,54 @@
+package Comporators;
+
+import classes.Hotel;
+import classes.Product;
+import comparators.HotelComporatorParity;
+import comparators.ProductComporatorParity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HotelComporatorParityTest {
+    @BeforeEach
+    public void setUp(){
+    }
+
+    @Test
+    @DisplayName("Тест Компоратора ProductComporatorParity")
+    public void testSort() {
+        HotelComporatorParity comporatorParity = new HotelComporatorParity();
+        List<Hotel> hotelList = new ArrayList<>();
+
+        Hotel hotel1 = new Hotel("Qwer1","Qwer",1);
+        Hotel hotel2 = new Hotel("Qwer2","Qwer",2);
+        Hotel hotel3 = new Hotel("Qwer3","Qwer",3);
+        Hotel hotel4 = new Hotel("Qwer4","Qwer",4);
+        Hotel hotel5 = new Hotel("Qwe5","Qwer",5);
+        Hotel hotel22 = new Hotel("Qwer22","Qwer",2);
+        Hotel hotel44 = new Hotel("Qwer44","Qwer",4);
+
+        hotelList.add(hotel3);
+        hotelList.add(hotel1);
+        hotelList.add(hotel4);
+        hotelList.add(hotel2);
+        hotelList.add(hotel5);
+        hotelList.add(hotel22);
+        hotelList.add(hotel44);
+        hotelList.sort(comporatorParity);
+        hotelList.forEach(t->System.out.println(t));
+
+        assertEquals(3, hotelList.get(0).getRating());
+        assertEquals(1, hotelList.get(1).getRating());
+        assertEquals(2, hotelList.get(2).getRating());
+        assertEquals(2, hotelList.get(3).getRating());
+        assertEquals(5, hotelList.get(4).getRating());
+        assertEquals(4, hotelList.get(5).getRating());
+        assertEquals(4, hotelList.get(6).getRating());
+
+    }
+}
---
+++ Komanda4/src/test/java/Comporators/UserComporatorParityTest.java
@@ -0,0 +1,53 @@
+package Comporators;
+
+import classes.User;
+import comparators.UserComporatorParity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.PushbackReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserComporatorParityTest {
+
+    @BeforeEach
+    public void setUp(){
+    }
+
+
+    @Test
+    @DisplayName("Тест Компоратора UserComporatorParity")
+    public void testSort() {
+        UserComporatorParity comporatorParity = new UserComporatorParity();
+        List<User> userList = new ArrayList<>();
+        User user1 = new User("Qwe1",1,"qwer@qwer.re");
+        User user2 = new User("Qwe2",2,"qwer@qwer.re");
+        User user3 = new User("Qwe3",3,"qwer@qwer.re");
+        User user4 = new User("Qwe4",4,"qwer@qwer.re");
+        User user5 = new User("Qwe5",5,"qwer@qwer.re");
+        User user6 = new User("Qwe6",6,"qwer@qwer.re");
+        User user7 = new User("Qwe7",7,"qwer@qwer.re");
+        userList.add(user6);
+        userList.add(user2);
+        userList.add(user1);
+        userList.add(user4);
+        userList.add(user3);
+        userList.add(user7);
+        userList.add(user5);
+        userList.sort(comporatorParity);
+        userList.forEach(t-> System.out.println(t));
+
+        assertEquals(2, userList.get(0).getAge());
+        assertEquals(4, userList.get(1).getAge());
+        assertEquals(1, userList.get(2).getAge());
+        assertEquals(6, userList.get(3).getAge());
+        assertEquals(3, userList.get(4).getAge());
+        assertEquals(7, userList.get(5).getAge());
+        assertEquals(5, userList.get(6).getAge());
+    }
+
+}
