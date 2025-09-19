ec292cceec6fe5b1fad11550b3e37138b347ce97
AZ1961T <andrei.302@yandex.ru>
2025-09-19 16:40:16 +0500

    add tests ComporatorParity 19/09/25

---
+++ Komanda4/src/test/java/Comporators/FlightComporatorParityTest.java
@@ -0,0 +1,54 @@
+package Comporators;
+
+import classes.Flight;
+import comparators.FlightComporatorParity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utils.Validator;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FlightComporatorParityTest {
+    @BeforeEach
+    public void setUp(){
+    }
+
+
+    @Test
+    @DisplayName("Тест Компоратора FlightComporatorParity")
+    public void testSort(){
+        FlightComporatorParity comporatorParity = new FlightComporatorParity();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+        LocalDateTime date1=LocalDateTime.parse("12.02.2000 00:00",formatter);
+        LocalDateTime date2=LocalDateTime.parse("13.02.2000 00:00",formatter);
+        LocalDateTime date3=LocalDateTime.parse("14.02.2000 00:00",formatter);
+        LocalDateTime date4=LocalDateTime.parse("15.02.2000 00:00",formatter);
+        LocalDateTime date5=LocalDateTime.parse("16.02.2000 00:00",formatter);
+
+        Flight flight1 = new Flight("Flightx",date1,123.456);
+        Flight flight2 = new Flight("Flightx2",date2,124.456);
+        Flight flight3 = new Flight("Flightx3",date3,125.456);
+        Flight flight4 = new Flight("Flightx4",date4,126.456);
+        Flight flight5 = new Flight("Flightx5",date5,127.456);
+
+        List<Flight> flightList = new ArrayList<>();
+        flightList.add(flight1);
+        flightList.add(flight5);
+        flightList.add(flight2);
+        flightList.add(flight4);
+        flightList.add(flight3);
+        flightList.sort(comporatorParity);
+        assertEquals(12, flightList.get(0).getDate().getDayOfMonth());
+        assertEquals(13, flightList.get(1).getDate().getDayOfMonth());
+        assertEquals(15, flightList.get(2).getDate().getDayOfMonth());
+        assertEquals(16, flightList.get(3).getDate().getDayOfMonth());
+        assertEquals(14, flightList.get(4).getDate().getDayOfMonth());
+
+    }
+}
---
+++ Komanda4/src/test/java/Comporators/ProductComporatorParityTest.java
@@ -0,0 +1,54 @@
+package Comporators;
+
+import classes.Product;
+import classes.User;
+import comparators.ProductComporatorParity;
+import comparators.UserComporatorParity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductComporatorParityTest {
+
+    @BeforeEach
+    public void setUp(){
+    }
+
+    @Test
+    @DisplayName("Тест Компоратора ProductComporatorParity")
+    public void testSort() {
+        ProductComporatorParity comporatorParity = new ProductComporatorParity();
+        List<Product> productList = new ArrayList<>();
+
+        Product product1 = new Product("Qwer1",1,10);
+        Product product2 = new Product("Qwer2",2,10);
+        Product product3 = new Product("Qwer3",3,10);
+        Product product4 = new Product("Qwer4",4,10);
+        Product product5 = new Product("Qwer5",5,10);
+        Product product6 = new Product("Qwer6",6,10);
+        Product product7 = new Product("Qwer7",7,10);
+
+        productList.add(product4);
+        productList.add(product1);
+        productList.add(product7);
+        productList.add(product6);
+        productList.add(product2);
+        productList.add(product5);
+        productList.add(product3);
+        productList.sort(comporatorParity);
+        //productList.forEach(t->System.out.println(t));
+
+        assertEquals(2, productList.get(0).getPrice());
+        assertEquals(1, productList.get(1).getPrice());
+        assertEquals(7, productList.get(2).getPrice());
+        assertEquals(4, productList.get(3).getPrice());
+        assertEquals(6, productList.get(4).getPrice());
+        assertEquals(5, productList.get(5).getPrice());
+        assertEquals(3, productList.get(6).getPrice());
+    }
+}
